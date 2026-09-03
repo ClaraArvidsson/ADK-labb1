2f863a5a6776dc1b879be403d5f8de759a7deb80
Clara Arvidsson <claraarv@kth.se>
2026-09-03 11:21:48 +0200

    almmost working

--- Tree.java
+++ Tree.java
@@ -1,24 +1,23 @@
 public class Tree {
-    private Root root;
+    private int size;
+    private Branch branch;
+
     public Tree(){
-        root = new Root();
+        size = 0;
     }
+
+    public Tree(Tree tree, int i, int value){
+        int height;
+        if (i < 2){
+           height = 2; 
+        }
+        height = (int) Math.ceil(Math.log(i)/Math.log(2));
+        branch = new Branch(height, i, value);
+    }
+
     
     private interface Node{
         public String toString();
-    }
-
-    private class Root{
-        private int size;
-        private Branch start;
-
-        public Root(){
-            size = 0;
-        }
-
-        public String toString(){
-            return start.toString();
-        }
     }
 
     private class Branch implements Node{
@@ -26,17 +25,17 @@ public class Tree {
         private Node left;
         private Node right;
 
-        public Branch(int height, int index, int value){
+        public Branch(int height, int i, int value){
             maxinsubtree = value;
-            int bit = (index << (height-1)) & 1;
-            if (height == 1){
+            int bit = (i >> (height-1)) & 1;
+            if (height <= 1){
                 if (bit == 0){
                     left = new Leaf(value);
                 } else if (bit == 1){
                     right = new Leaf(value);
                 }
             } else {
-                Branch newBranch = new Branch(height-1, index, value);
+                Branch newBranch = new Branch(height-1, i, value);
                 if (bit == 0){
                     left = newBranch;
                 } else if (bit == 1){
@@ -46,7 +45,9 @@ public class Tree {
         }
 
         public String toString(){
-            return "(" + left.toString() + " " + right.toString() + ")";
+            String leftString = left != null ? left.toString() : "null";
+            String rightString = right != null ? right.toString() : "null";
+            return "(" + leftString + " " + rightString + ")";
         }
     }
 
@@ -63,8 +64,23 @@ public class Tree {
     }
 
     public String toString(){
-        return root.toString();
+        return branch.toString();
+    }
+
+    public static Tree newarray(){
+        return new Tree();
+    }
+
+    public static Tree set(Tree a, int i, int value){
+        return new Tree(a, i, value);
+    }
+
+    public static void main(String[] args){
+        Tree tree = newarray();
+        tree = set(tree, 5, 12);
+        System.out.println(tree.toString());
     }
 }
+
 
 
